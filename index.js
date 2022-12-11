247a8b4ee1f8bece9c2bd9ea100fec82ff755e54
Lazar Dedic <48831788+Lamzur@users.noreply.github.com>
2022-12-11 03:30:02 +0100

    First commit
    Added the .html files along with .pngs and some js and css

---
+++ index.js
@@ -0,0 +1,47 @@
+function hover1(slika) {
+    slika.setAttribute('src', 'slidze/projectsclick.png');
+}
+
+function unhover1(slika) {
+    slika.setAttribute('src', 'slidze/projects.png');
+}
+
+function hover2(slika) {
+    slika.setAttribute('src', 'slidze/lazarigramprojectclick.png');
+}
+
+function unhover2(slika) {
+    slika.setAttribute('src', 'slidze/lazarigramproject.png')
+}
+
+function hover3(slika) {
+    slika.setAttribute('src', 'slidze/coversclick.png');
+}
+
+function unhover3(slika) {
+    slika.setAttribute('src', 'slidze/covers.png');
+}
+
+function hover4(slika) {
+    slika.setAttribute('src', 'slidze/foodclick.png');
+}
+
+function unhover4(slika) {
+    slika.setAttribute('src', 'slidze/food.png');
+}
+
+function hover5(slika) {
+    slika.setAttribute('src', 'slidze/codingclick.png');
+}
+
+function unhover5(slika) {
+    slika.setAttribute('src', 'slidze/coding.png');
+}
+
+function hover6(slika) {
+    slika.setAttribute('src', 'slidze/animationsclick.png');
+}
+
+function unhover6(slika) {
+    slika.setAttribute('src', 'slidze/animations.png');
+}
